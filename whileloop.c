97d4d9ded634dca4284d9ccc35848230c8725b24
Lansmoke <11344lawrencekigotho@gmail.com>
2024-10-08 11:50:49 +0300

    initial commit

---
+++ whileloop.c
@@ -0,0 +1,49 @@
+/*Program to run while loop
+Author: Lawrence Kigotho Njoroge
+Reg No: CT101/G/21996/24
+*/
+
+#include<stdio.h>
+int main() {
+	int start;
+	int stop;
+	int sum;
+	
+	//Ask user for a starting point
+	printf("Enter the starting point: ");
+	scanf("%d", &start);
+	
+	//Ask user to enter end point
+	printf("Enter the end point: ");
+	scanf("%d", &stop);
+	
+	//introduce while loop
+	while(start<=stop) {
+		start++;
+		
+		//formula for sum
+		sum =sum + stop;
+	}
+	
+	//print sum value
+	printf("The sum is %d\n", sum);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
