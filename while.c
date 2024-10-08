714eeb4a0dfc8c66d5ccea799b8556b6d4e1dd1b
Lansmoke <11344lawrencekigotho@gmail.com>
2024-10-08 12:56:05 +0300

    initial commit

---
+++ while.c
@@ -0,0 +1,23 @@
+/* program for while
+Author: Lawrence Njoroge
+Reg N0:CT101/G/21996/24
+*/
+
+#include<stdio.h>
+int main () {
+	int i=i;
+	int sum=0;
+	
+	while (i<=20) 
+	{
+		printf("%d\n", i);
+		i++;
+		
+		sum = sum + i;
+	}
+	
+	printf("The sum is %d\n", sum);
+	
+	
+	return 0;
+}
